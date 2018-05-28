32cca444c6d82818648abf90492fc41878d9f8ed
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-28 15:32:31 +0200

    challenge quete010

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.updateEmailField = this.updateEmailField.bind(this);
+  }
+
+  updateEmailField(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
       <div>
-        <h1>test@test.com</h1>
-        <input type="email" name="email" />
+        <h1>{this.state.value}</h1>
+        <input
+          type="email"
+          name="email"
+          value={this.state.value}
+          onChange={this.updateEmailField}
+        />
       </div>
     );
   }
