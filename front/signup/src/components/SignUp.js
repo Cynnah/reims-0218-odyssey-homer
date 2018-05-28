3cccbfeb1b255dda2254bf9191b9c2502a891049
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-05-28 15:49:03 +0200

    Quete 010

---
+++ front/signup/src/components/SignUp.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+
+    this.updateEmailField = this.updateEmailField.bind(this);
+  }
+
+  updateEmailField(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <h1> {this.state.value}</h1>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.updateEmailField}
+        />
+      </form>
+    );
+  }
+}
+
+export default SignUp;
