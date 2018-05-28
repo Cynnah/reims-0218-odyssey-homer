eb7f4d05a2c388e859999f5b6ee5380f8b5579a5
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-28 15:10:31 +0200

    add text and input

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -1,8 +1,13 @@
-import React, { Component, input } from "react";
+import React, { Component } from "react";
 
 class SignUp extends Component {
   render() {
-    return <input type="email" name="email" />;
+    return (
+      <div>
+        <h1>test@test.com</h1>
+        <input type="email" name="email" />
+      </div>
+    );
   }
 }
 
