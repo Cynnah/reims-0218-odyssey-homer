1e0619e0f8d109e3d141471c76ef5157f6814ccc
Cynnah <massot.dorian@gmail.com>
2019-07-24 12:44:43 +0200

    creation on a form and display in console in string

--- front/homere/src/components/SignUp.js
+++ front/homere/src/components/SignUp.js
@@ -1,30 +1,66 @@
 import React, { Component } from "react";
 
 class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
+  
+  state = {
+      email: '',
+      password: '',
+      verificationPassword: '',
+      firstname: '',
+      lastname: ''
     };
+  
 
-    this.updateEmailField = this.updateEmailField.bind(this);
+  updateEmailField = (event) => {
+    this.setState({ 
+      [event.target.name]: event.target.value 
+    });
   }
 
-  updateEmailField(event) {
-    this.setState({ value: event.target.value });
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(JSON.stringify(this.state))
   }
 
   render() {
+
+    const email = this.state.email;
+    const password = this.state.password;
+    const verificationPassword = this.state.verificationPassword;
+    const firstname = this.state.firstname;
+    const lastname = this.state.lastname;
+    const handleChanges = this.updateEmailField;
+    const handleSubmit = this.handleSubmit;
+
     return (
-      <div>
-        <form>
-          <h1>{this.state.value}</h1>
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.updateEmailField}
-          />
-          email
+      <div className="signup">
+        <form onSubmit={handleSubmit}>
+          <h3>{email}</h3>
+          <label>Email : <br />
+            <input type="email" name="email" onChange={handleChanges} />
+          </label>
+
+          <h3>{password}</h3>
+          <label>Mot de passe : <br />
+            <input type="password" name="password" onChange={handleChanges} />
+          </label>
+
+          <h3>{verificationPassword}</h3>
+          <label>Vérification : <br />
+            <input type="password" name="verificationPassword" onChange={handleChanges} />
+          </label>
+
+          <h3>{firstname}</h3>
+          <label>Prénom : <br />
+            <input type="text" name="firstname" onChange={handleChanges} />
+          </label>
+
+          <h3>{lastname}</h3>
+          <label>Nom : <br />
+            <input type="text" name="lastname" onChange={handleChanges} />
+          </label><br />
+
+          <input type="submit" value="Soumettre" />
         </form>
       </div>
     );
